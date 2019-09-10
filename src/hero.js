13c351bbd32bbc511707deb1fa0047cbd1fc1139
samjgorman <samjgorman@gmail.com>
2019-09-10 16:44:51 -0700

    basic styling hero

--- src/hero.js
+++ src/hero.js
@@ -2,18 +2,36 @@ import React, {Component} from 'react';
 import styled, { css } from 'styled-components'
 import './hero.css';
 
-
-const Wrapper = styled.section`
+//MQ here at 500px 
+const Wrapper = styled.div`
     display: flex;
     flex-direction: row;
+    margin-right: 4em;
+    margin-left: 4em;
+
+    @media (max-width: 500px) {
+      flex-direction: column;
+      margin-right: 2em;
+      margin-left: 2em;
+      
+    }
+
+    @media (max-width: 800px) {
+      flex-direction: column;
+     
+      
+    }
 `;
 
 const Text = styled.div`
     color: ${props => props.color};
+    font-family: Overpass;
     font-weight: 800;
-    font-size: 32px;
+    font-size: 26px;
+    margin-bottom: 1em;
+    padding-right: 2em;
+   
 
-    
 `;
 
 const Image = styled.div`
@@ -23,6 +41,14 @@ const Image = styled.div`
 const TextWrapper = styled.div`
     display: flex;
     flex-direction: column;
+    padding-top: 10em;
+    padding-bottom: 10em;
+
+    @media (max-width: 800px) {
+     padding-bottom: 4em;
+     text-align: center;
+      
+    }
 `;
 
 const Hero = ( {textOne, textTwo,image, color} ) => {
@@ -30,7 +56,7 @@ const Hero = ( {textOne, textTwo,image, color} ) => {
     <Wrapper> 
       <TextWrapper> 
         <Text textOne = {textOne} color = {color}> {textOne} </Text>
-        <Text textTwo = {textTwo}> {textTwo} </Text>
+        <Text textTwo = {textTwo} color = {color}> {textTwo} </Text>
       </TextWrapper>
       <img className = "hero-img" src ={require(`${image}`)} /> 
     </Wrapper>
