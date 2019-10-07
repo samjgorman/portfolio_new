f912f78e16bb4c3efade8bb8175f678d30172f08
samjgorman <samjgorman@gmail.com>
2019-10-07 01:35:12 -0700

    learnings, structure

--- src/containers/figma.js
+++ src/containers/figma.js
@@ -22,7 +22,6 @@ import rc from '../images/rc-final.gif';
 
 
 class Figma extends Component {
-  //adding constructor from tut
   constructor(props){
     super(props);
     //now assign state, and a default value, doesnt matter if i say data
@@ -30,7 +29,6 @@ class Figma extends Component {
       //contacts: []
     };
   }
- 
 
   render() {
   return (
@@ -93,11 +91,6 @@ class Figma extends Component {
     ></Title>
 
 
-    <Quote
-    text = "If you can build a website, you should be able to make a plugin."
-    color ="#1bc47d"
-    ></Quote>
-
     <Title
     tag = "USER RESEARCH"
     color = "#1bc47d"
@@ -112,20 +105,63 @@ class Figma extends Component {
       image = "./images/figma-users@2x.png"
      ></SmallImage>
 
+     <Title
+     text = "From these users, it appeared there were three main problems with the current plugin design."
+     ></Title>
+
+    < Paragraph
+     text = "1.  The lack of context on what plugins can and can't do make exploration difficult."
+     ></Paragraph>
+
+    <Paragraph
+     text = "2.  It was challenging to find the right Figma plugin for the right use case."
+     ></Paragraph>
+
+    <Paragraph
+     text = "3.  The product was falling short in supporting designers with ideas to create plugins."
+     ></Paragraph>
+
+     <Divider
+        title = "Enabling exploration on Plugins home"
+        color = "#5551FF"
+        textcolor = "white"
+     ></Divider>
+
     <SmallImage
       image = "./images/davidsad@2x.png"
      ></SmallImage>
 
     <SmallImage
+      image = "./images/figma-home@2x.png"
+      caption = "The full plugin home page."
+     ></SmallImage>
+
+    <Divider
+        title = "Designing a context-based search "
+        color = "#5551FF"
+        textcolor = "white"
+     ></Divider>
+
+
+    <SmallImage
       image = "./images/shalinsad@2x.png"
      ></SmallImage>
+
+    <Divider
+        title = "Putting creators first: a source of truth for plugin development "
+        color = "#5551FF"
+        textcolor = "white"
+     ></Divider>
+
 
     <SmallImage
       image = "./images/michellesad@2x.png"
      ></SmallImage>
 
-
-
+    <Quote
+    text = "If you can build a website, you should be able to make a plugin."
+    color ="#1bc47d"
+    ></Quote>
 
 
      <SmallImage
@@ -134,19 +170,54 @@ class Figma extends Component {
      ></SmallImage>
 
     <SmallImage
-      image = "./images/figma-home@2x.png"
-      caption = "The full plugin home page."
-     ></SmallImage>
-
-<SmallImage
       image = "./images/figma-sketches@2x.png"
       caption = "Bad handwriting or folksy charm?."
      ></SmallImage>
 
+    <Divider
+        title = "Learnings"
+        color = "#5551FF"
+        textcolor = "white"
+     ></Divider>
 
-   
+     <Title
+     text = "Stay human-driven, no matter the timeline."
+     tag = "ONE"
+     color = "#1bc47d"
+     ></Title>
 
-     
+    <Paragraph
+    text = "I had four days to execute on this project.  There wasn't enough time to do the sort of intensive needfinding and synthesis I've gotten used to. "
+    >
+     </Paragraph>
+
+     <Paragraph
+     text = "This meant that I had to hack ways to build empathy with Figma's users.  This framing led me to interview fellow designers on day one of the project, reach out in community forums for feedback, and read spotlights on creators from Figma's blog. "
+    >
+     </Paragraph>
+
+
+    <Title
+     text = "Redesign, with 110% intention."
+     tag = "TWO"
+     color = "#1bc47d"
+     ></Title>
+
+     <Paragraph
+     text = "Many designers critique external redesign projects, and they're not fully wrong.  I, Sam Gorman, am good designer (I think).  But I won't pretend I somehow have more insight during a four-day sprint than the entire product team of Figma combined."
+    >
+     </Paragraph>
+
+     <Paragraph
+     text = "I view the role of this redesign as a provocation, not a definitive answer.  I focused heavily on redesigning the UX, within the UI constraints Figma already established.  I hope this makes the redesign more feasible, and can spark more of a substantive product discussion on Figma Plugins."
+    >
+     </Paragraph>
+
+    <DividerEnd
+        color = "#5551FF"
+     ></DividerEnd>
+
+
 
     <Footer/>
 
