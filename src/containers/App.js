537efb1b87c43bff3e0a9dd57de457a8a5f01020
samjgorman <samjgorman@gmail.com>
2019-10-12 18:19:46 -0700

    new ordered to home

--- src/containers/App.js
+++ src/containers/App.js
@@ -39,6 +39,13 @@ font-size: 24px;
   font-size: 18px;
 }`;
 
+const Background= styled.div`
+background-color: #FCDDBC;
+margin-left: 10%;
+margin-right: 10%;
+
+}`;
+
 class App extends Component {
   //adding constructor from tut
   constructor(props){
@@ -60,28 +67,41 @@ class App extends Component {
     <HeroSub> I’m a designer, entrepreneur, and undergrad at Stanford.</HeroSub>
     </HeroContainer>
 
-    <FeatureRev
+    <Feature
+    
+
+     image = "./images/figma@2x.png"
+    tag = "UI DESIGN"
+    title = "Figma Plugins"
+    text = "A redesign of the popular design software for Kleiner Perkins Fellows."
+    shade = "85,81,255, 0.5"
+    color = "#5551FF"
+    link = "/figma-kp"
+ 
+    ></Feature>
+
+    <FeatureBk
     image = "./images/pink@2x.png"
     tag = "FULL STACK"
-    title = "Peerlift Connect"
+    title = "Peerlift"
     text = "A human way to discover scholarships and summer programs."
     shade = "255, 109, 150, 0.5"
     color = "#FF6D96"
     link = "/connect"
-    ></FeatureRev>
+    background = "#FCEADE"
+    ></FeatureBk>
 
-
-
-    <FeatureBk
+    <Feature
     image = "./images/microfeature@2x.png"
     tag = "UI DESIGN"
     title = "Microscholarships"
-    text = "Building scholarships of the future for 60,000+ users and counting."
+    text = "A pivot from Peerlift:  enabling anyone to create and win scholarships."
     shade = "46, 141, 241, 0.5"
     color = "#2e8df1"
     link = "/case"
-    background = "#ffd692"
-    ></FeatureBk>
+
+    ></Feature>
+
 
     <Feature
     image = "./images/RCfeature@2x.png"
@@ -91,16 +111,6 @@ class App extends Component {
     shade = "0, 206, 161, 0.5"
     color = "#00cea1"
     link = "/random"
-    ></Feature>
-
-    <Feature
-    image = "./images/figma@2x.png"
-    tag = "UI DESIGN"
-    title = "Figma Plugins"
-    text = "A redesign of the popular design software for Kleiner Perkins Fellows."
-    shade = "85,81,255, 0.5"
-    color = "#5551FF"
-    link = "/figma-kp"
     ></Feature>
 
    
